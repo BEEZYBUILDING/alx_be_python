856a30dedb10f7a75846e6b873ee31ee10582ea7
OLAJIRE <121580186+BEEZYBUILDING@users.noreply.github.com>
2025-05-23 19:42:45 +0100

    Update simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,1 +1,8 @@
 #This is the second python project.
+principal = 1000 # represents $1000
+rate = 0.05 # represents 5%
+time = 3 #represents 3 years
+
+interest = principal * rate * time
+
+print("The simple interest:", interest)
