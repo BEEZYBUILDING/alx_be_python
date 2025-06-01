377f3c695d8c766267e783d5449fa61896fa02a1
OLAJIRE <121580186+BEEZYBUILDING@users.noreply.github.com>
2025-06-01 22:15:48 +0100

    Update multiplication_table.py

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,1 +1,7 @@
-week 4.2 project
+#week 4.2 project
+number = int(input("Enter a number to see its multiplication table: ")
+
+for i in range(9):
+i = i + 1:
+num = number * i:
+print(number, "*", i, "=", num)
