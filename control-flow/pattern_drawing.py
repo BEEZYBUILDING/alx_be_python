31c7b4f90fc79d70692e6906d56ca43bbf0700d2
OLAJIRE <121580186+BEEZYBUILDING@users.noreply.github.com>
2025-06-01 22:48:21 +0100

    Update pattern_drawing.py

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,1 +1,15 @@
-week 4.3 project
+#week 4.3 project
+size = int(input("Enter the size of the pattern: "))
+row = 0
+
+while row < size:
+     
+    for i in range(size): # Use a for loop to print asterisks in a row
+        print("*", end="")  # Stay on the same line
+    print()  # Move to the next line after each row
+    row += 1
+
+
+
+
+
