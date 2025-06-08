fbeb7dff81e652942dc109a3e4f1d1b8c5c187fe
OLAJIRE <121580186+BEEZYBUILDING@users.noreply.github.com>
2025-06-08 22:36:34 +0100

    Update temp_conversion_tool.py

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -8,7 +8,7 @@ def convert_to_celsius(fahrenheit):
     return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
 
 def convert_to_fahrenheit(celsius):
-    return (celsius + 32) * CELSIUS_TO_FAHRENHEIT_FACTOR
+    return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
 
 try:
     temp = float(input("Enter the temperature to convert: "))
