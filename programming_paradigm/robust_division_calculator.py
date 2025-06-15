1201e8700eecb1bd9187f1201a173c6cfbfa5895
OLAJIRE <olajirefolasore@gmail.com>
2025-06-15 22:35:18 +0100

    add calculator to paradigm

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -2,7 +2,7 @@
 def safe_divide(numerator, denominator):
     try:
         result = float(numerator) / float(denominator)
-        print(f'The result of the division is {result}')
+        return f'The result of the division is {result}'
     except ZeroDivisionError:
         return "Error: Cannot divide by zero."
     except ValueError:
